d2439956e361b4ca012e51c6caee8c6037d3782d
SaTyEndRa <84174959+S4TyEndRa@users.noreply.github.com>
2022-03-18 17:48:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 class vars(object):
-    api_id = os.getenv('API_ID', 123456)
-    api_hash = os.getenv('API_HASH' ,'abcdefghxxxxx')
-    bot_token = os.getenv('BOT_TOKEN', None)
-    max = int(os.getenv('MAX', 20))
+    api_id = 2171111
+    api_hash = "fd7acd07303760c52dcc0ed8b2f73086"
+    bot_token = "5263754363:AAFdmNpmqjZj3d0psXivJR11ni54mXmUWPU"
+    max = 20
